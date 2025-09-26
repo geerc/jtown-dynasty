d66d22130401bc76032023ba2f7421a59deaa4b0
Christian Geer <geerc@allegheny.edu>
2025-09-26 14:50:47 -0400

    site deploy debuggin

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,19 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
 echo "Downloading Cecil"
-if [ -z $CECIL_VERSION ]; then
+if [ -z "$CECIL_VERSION" ]; then
   curl -sSOL https://cecil.app/cecil.phar
 else
   curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
+
 php cecil.phar --version
 
 echo "Started Cecil build"
+
+# Use DEPLOY_PRIME_URL for preview builds, SITE_URL for production (set in Netlify env)
+BASEURL="${DEPLOY_PRIME_URL:-$URL}"
+
+# Build site
 if [[ $1 == "preview" ]]; then
-  php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts
+  php cecil.phar build -v --baseurl="$BASEURL" --drafts
 else
-  php cecil.phar build -v --baseurl=$URL --postprocess
+  php cecil.phar build -v --baseurl="$BASEURL" --postprocess
 fi
 
-# build success? can deploy?
-if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
-
-echo "Interrupted Cecil build"; exit 1
+echo "Finished Cecil build"
